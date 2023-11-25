ebcc72f73a747dedf67c1369432f4cb915f4c2a7
Rui Borges <ruiborges206@gmail.com>
2023-11-25 14:02:46 +0000

    Added score to surfer and created setter for Position in Element

--- src/main/java/org/crazytracks/model/Element.java
+++ src/main/java/org/crazytracks/model/Element.java
@@ -9,4 +9,8 @@ public class Element{
     public Position getPosition(){
         return position;
     }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
