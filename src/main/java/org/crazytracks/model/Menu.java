c94d23b2974942fcf100f8925fc9abb03d20ced0
Rui Borges <ruiborges206@gmail.com>
2023-11-25 14:53:04 +0000

    Added model for Menu and removed attributes from model viewer

--- src/main/java/org/crazytracks/model/Menu.java
+++ src/main/java/org/crazytracks/model/Menu.java
@@ -1,5 +1,33 @@
 package org.crazytracks.model;
 
-public class Menu{
+import java.util.Arrays;
+import java.util.List;
 
+public class Menu{
+    private final List<String> entries;
+    private int currentEntry = 0;
+
+    public Menu() {
+        this.entries = Arrays.asList("Start", "Exit");
+    }
+
+    public String getCurrentEntry() {
+        return entries.get(currentEntry);
+    }
+
+    public void nextEntry() {
+        currentEntry = (currentEntry + 1) % entries.size();
+    }
+
+    public void previousEntry() {
+        currentEntry = (currentEntry - 1 + entries.size()) % entries.size();
+    }
+
+    public String getEntry(int i) {
+        return entries.get(i);
+    }
+
+    public boolean IsSelected() {
+        return currentEntry == 0;
+    }
 }
--- src/main/java/org/crazytracks/viewer/MenuViewer.java
+++ src/main/java/org/crazytracks/viewer/MenuViewer.java
@@ -1,12 +1,6 @@
 package org.crazytracks.viewer;
 
 public class MenuViewer implements Viewer {
-    private int option;
-
-    public MenuViewer() {
-        this.option = 0;
-    }
-
     @Override
     public void updateGUI() {
         // update menu
