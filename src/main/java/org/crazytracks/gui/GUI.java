2dba0104979dc7a5d57ed91b1567890aa102f9e9
Daniel Basílio <dbasilio15@gmail.com>
2023-11-30 18:16:27 +0000

    Gave a new look to the GUI

--- src/main/java/org/crazytracks/gui/GUI.java
+++ src/main/java/org/crazytracks/gui/GUI.java
@@ -4,12 +4,12 @@ import org.crazytracks.model.Position;
 
 public interface GUI {
     void initGameGUI();
-
     void drawCoin(Position position);
 
     void drawSurfer(Position position);
     void drawPowerUp(Position position);
     void drawWagon(Position position);
     void putScore(int score);
+    void putMultiplier(int powerUpValue);
 
 }
--- src/main/java/org/crazytracks/gui/LanternaGUI.java
+++ src/main/java/org/crazytracks/gui/LanternaGUI.java
@@ -12,43 +12,82 @@ import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 import org.crazytracks.model.Position;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class LanternaGUI implements GUI {
     private final Screen screen;
     private final int terminalWidth;
     private final int terminalHeight;
+    private final int numLanes = 3;
     private int leftMargin;
+    private final TextColor trackColor = new TextColor.RGB(188,187,156);
     PositionAdapter positionAdapter;
-    public LanternaGUI(int terminalWidth, int terminalHeight) throws IOException {
+    public LanternaGUI(int terminalWidth, int terminalHeight) throws IOException, URISyntaxException, FontFormatException {
         this.leftMargin = 14;
         this.terminalWidth = terminalWidth;
         this.terminalHeight = terminalHeight;
 
-        Font myFont = new Font("Monospaced", Font.PLAIN, 20);
-        AWTTerminalFontConfiguration myFontConfiguration = AWTTerminalFontConfiguration.newInstance(myFont);
-        DefaultTerminalFactory dtf = new DefaultTerminalFactory()
-                .setInitialTerminalSize(new TerminalSize(terminalWidth, terminalHeight));
-        dtf.setForceAWTOverSwing(true);
-        dtf.setTerminalEmulatorFontConfiguration(myFontConfiguration);
-        Terminal terminal = dtf.createTerminal();
+        Terminal terminal = terminalCreation(terminalWidth, terminalHeight);
 
         this.screen = new TerminalScreen(terminal);
         this.screen.startScreen();
     }
 
+    public Terminal terminalCreation(int terminalWidth, int terminalHeight) throws IOException, FontFormatException, URISyntaxException {
+//        URL resource = getClass().getClassLoader().getResource("fonts/AnonymousPro-Regular.ttf");
+        URL resource = getClass().getClassLoader().getResource("fonts/square.ttf");
+        File fontFile = new File(resource.toURI());
+        Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+
+        Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+        factory.setTerminalEmulatorFontConfiguration(fontConfig);
+        factory.setForceAWTOverSwing(true);
+        factory.setInitialTerminalSize(new TerminalSize(terminalWidth, terminalHeight));
+        return factory.createTerminal();
+    }
+
     @Override
     public void initGameGUI() {
-
-        int trackWidth = 3;
         int trackHeight = this.terminalHeight;
 
-        TextCharacter solidBlock = new TextCharacter(' ').withBackgroundColor(TextColor.ANSI.WHITE);
-
-        for (int y = 0; y < trackHeight; y++) {
-            for (int x = this.leftMargin; x < this.leftMargin + trackWidth; x++) {
+        // Paint the grass
+        TextCharacter solidBlock = new TextCharacter(' ').withBackgroundColor(TextColor.ANSI.GREEN);
+        for (int y = 0; y < this.terminalHeight; y++) {
+            for (int x = 0; x < this.terminalWidth; x++) {
                 screen.setCharacter(x, y, solidBlock);
             }
+        }
+
+        // Paint the track
+        solidBlock = new TextCharacter('H')
+                .withForegroundColor(TextColor.ANSI.WHITE)
+                .withBackgroundColor(trackColor);
+        for (int y = 0; y < trackHeight; y++) {
+            for (int x = this.leftMargin; x < this.leftMargin + this.numLanes; x++) {
+                screen.setCharacter(x, y, solidBlock);
+            }
+        }
+
+        // Paint the borders of track
+        solidBlock = new TextCharacter(':')
+                .withForegroundColor(TextColor.ANSI.BLACK)
+                .withBackgroundColor(TextColor.ANSI.GREEN);
+        int x = this.leftMargin-1;
+        for (int y = 0; y < this.terminalHeight; y++) {
+            screen.setCharacter(x, y, solidBlock);
+        }
+        x = this.leftMargin + this.numLanes;
+        for (int y = 0; y < this.terminalHeight; y++) {
+            screen.setCharacter(x, y, solidBlock);
         }
 
         try {
@@ -68,15 +107,15 @@ public class LanternaGUI implements GUI {
 
     @Override
     public void drawSurfer(Position position) {
-        TextCharacter surferCharacter = new TextCharacter('O')
+        TextCharacter surferCharacter = new TextCharacter('S')
                 .withForegroundColor(TextColor.ANSI.BLUE)
-                .withBackgroundColor(TextColor.ANSI.WHITE);
+                .withBackgroundColor(trackColor);
         putCharacter(position, surferCharacter);
     }
 
     @Override
     public void drawPowerUp(Position position) {
-        TextCharacter powerUpCharacter = new TextCharacter('↑')
+        TextCharacter powerUpCharacter = new TextCharacter('p')
                 .withForegroundColor(TextColor.ANSI.GREEN)
                 .withBackgroundColor(TextColor.ANSI.WHITE);
         putCharacter(position, powerUpCharacter);
@@ -84,7 +123,7 @@ public class LanternaGUI implements GUI {
 
     @Override
     public void drawWagon(Position position) {
-        TextCharacter wagonCharacter = new TextCharacter('H')
+        TextCharacter wagonCharacter = new TextCharacter('|')
                 .withForegroundColor(TextColor.ANSI.BLACK)
                 .withBackgroundColor(TextColor.ANSI.RED);
         putCharacter(position, wagonCharacter);
@@ -107,8 +146,27 @@ public class LanternaGUI implements GUI {
         TextGraphics textGraphics = screen.newTextGraphics();
         int x = 2;
         int y = 2;
-        textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
+        textGraphics
+                .setForegroundColor(TextColor.ANSI.BLACK)
+                .setBackgroundColor(TextColor.ANSI.GREEN);
         textGraphics.putString(x, y, "Score: " + String.valueOf(score));
+
+        try {
+            screen.refresh();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void putMultiplier(int powerUpValue) {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        int x = this.terminalWidth - 6;
+        int y = 2;
+        textGraphics
+                .setForegroundColor(TextColor.ANSI.BLACK)
+                .setBackgroundColor(TextColor.ANSI.GREEN);
+        textGraphics.putString(x, y, "X" + String.valueOf(powerUpValue));
 
         try {
             screen.refresh();
