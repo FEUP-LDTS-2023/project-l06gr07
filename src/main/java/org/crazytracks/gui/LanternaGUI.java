0af9714bf6738e6e91f51a716bc8a9a6b9cc07e2
Daniel Basílio <dbasilio15@gmail.com>
2023-12-01 16:37:29 +0000

    working on the track animation in menu GUI

--- src/main/java/org/crazytracks/gui/LanternaGUI.java
+++ src/main/java/org/crazytracks/gui/LanternaGUI.java
@@ -70,10 +70,10 @@ public class LanternaGUI implements GUI {
             }
         }
         TextColor borderColor = TextColor.ANSI.WHITE;
-        paintTrack(this.leftMargin, borderColor);
+        drawTrack(this.leftMargin, 0, borderColor);
     }
 
-    public void paintTrack(int xMargin, TextColor borderColor){
+    public void drawTrack(int xMargin, int animMode, TextColor borderColor){
         // Paint the track
         TextCharacter block = new TextCharacter('H')
                 .withForegroundColor(TextColor.ANSI.WHITE)
@@ -83,7 +83,7 @@ public class LanternaGUI implements GUI {
                 screen.setCharacter(x, y, block);
             }
         }
-        paintBorders(xMargin, 0, borderColor);
+        paintBorders(xMargin, animMode, borderColor);
     }
 
     public void paintBorders(int xMargin, int animMode, TextColor bgColor){
@@ -155,9 +155,7 @@ public class LanternaGUI implements GUI {
     }
 
     private void putCharacter(Position position, TextCharacter wagonCharacter) {
-        PositionAdapter positionAdapter = new PositionAdapter(this.leftMargin, this.terminalHeight-1);
-        Position adaptedPosition = positionAdapter.adaptPosition(position);
-        screen.setCharacter(adaptedPosition.getX(), adaptedPosition.getY(), wagonCharacter);
+        screen.setCharacter(position.getX(), position.getY(), wagonCharacter);
 
         try {
             screen.refresh();
@@ -212,7 +210,7 @@ public class LanternaGUI implements GUI {
         paintOptions(textLeftMargin, textTopMargin + logoTopMargin, options, selected);
 
         TextColor borderColor = TextColor.ANSI.GREEN;
-        paintTrack(trackLeftMargin, borderColor);
+        TrackAnimation animTrack = new TrackAnimation(this, trackLeftMargin, borderColor, this.terminalHeight);
     }
 
     public void paintLogo(int xMargin, int yMargin){
@@ -255,5 +253,9 @@ public class LanternaGUI implements GUI {
                 screen.setCharacter(x, y, solidBlock);
             }
         }
+    }
+
+    public int getTerminalHeight() {
+        return this.terminalHeight;
     }
 }
--- src/main/java/org/crazytracks/gui/TrackAnimation.java
+++ src/main/java/org/crazytracks/gui/TrackAnimation.java
@@ -1,4 +1,75 @@
 package org.crazytracks.gui;
 
+import com.googlecode.lanterna.TextColor;
+import org.crazytracks.model.Position;
+import org.crazytracks.model.TrackElement;
+import org.crazytracks.model.Wagon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrackAnimation {
+    private final int xMargin;
+    private final TextColor borderColor;
+    private final GUI gui;
+    private int animMode;
+    private int trackHeight;
+    private List<TrackElement> trackElements;
+
+    public TrackAnimation(GUI gui, int xMargin, TextColor borderColor, int trackHeight){
+        this.gui = gui;
+        this.xMargin = xMargin;
+        this.trackHeight = trackHeight;
+        this.animMode = 0;
+        this.borderColor = borderColor;
+        this.trackElements = loadTrackList();
+        initAnimation();
+        for (int i = 0; i < 10; i++){
+            step();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void step(){
+        this.animMode = (this.animMode + 1)%2;
+        decrementYPos();
+        gui.drawTrack(xMargin, animMode, borderColor);
+        drawTrackElements();
+    }
+
+    private void initAnimation(){
+        gui.drawTrack(xMargin, animMode, borderColor);
+    }
+
+    private List<TrackElement> loadTrackList(){
+        List<TrackElement> loadedTrack = new ArrayList<>();
+
+        loadedTrack.add(new Wagon(new Position(2, 25)));
+        loadedTrack.add(new Wagon(new Position(2, 26)));
+        loadedTrack.add(new Wagon(new Position(2, 27)));
+        loadedTrack.add(new Wagon(new Position(2, 28)));
+
+        return loadedTrack;
+    }
+    private void decrementYPos(){
+        for (int i = 0; i < trackElements.size(); i++){
+            TrackElement te = trackElements.get(i);
+            if (te.getPosition().getY() > 0){
+                te.setPosition(new Position(te.getPosition().getX(), te.getPosition().getY()-1));
+            } else {
+                trackElements.remove(i);
+            }
+        }
+    }
+    private void drawTrackElements(){
+        for (TrackElement te : trackElements){
+            PositionAdapter positionAdapter = new PositionAdapter(this.xMargin, this.trackHeight-1);
+            Position adaptedPosition = positionAdapter.adaptPosition(te.getPosition());
+            gui.drawWagon(adaptedPosition);
+        }
+    }
+
 }
