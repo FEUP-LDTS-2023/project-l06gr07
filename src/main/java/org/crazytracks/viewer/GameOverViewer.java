1d795a8f00423d171e2f90d7cc2b32cd7f48d54b
Rui Borges <ruiborges206@gmail.com>
2023-12-04 23:48:19 +0000

    Fixing merge conflicts

--- src/main/java/org/crazytracks/viewer/GameOverViewer.java
+++ src/main/java/org/crazytracks/viewer/GameOverViewer.java
@@ -4,7 +4,6 @@ import org.crazytracks.gui.GUI;
 import org.crazytracks.model.Element;
 
 public class GameOverViewer implements ElementViewer {
-
     @Override
     public void draw(Element element, GUI gui) {
 
--- src/main/java/org/crazytracks/viewer/Viewer.java
+++ src/main/java/org/crazytracks/viewer/Viewer.java
@@ -2,7 +2,7 @@ package org.crazytracks.viewer;
 
 import org.crazytracks.gui.GUI;
 
-<<<<<<< HEAD
+
 import java.io.IOException;
 
 public abstract class Viewer<T> {
@@ -23,8 +23,4 @@ public abstract class Viewer<T> {
     }
 
     protected abstract void drawElements(GUI gui) throws IOException;
-=======
-public interface Viewer {
-    public void draw(GUI gui);
->>>>>>> 3cb26f2aa4fae63373b032e8ee4b20dacd93d81d
 }
