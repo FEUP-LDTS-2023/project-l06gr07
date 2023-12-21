27cffa071c4de63330284a5610cddcd9920d1df0
Daniel Basílio <dbasilio15@gmail.com>
2023-12-21 23:29:50 +0000

    compatibility for sound when javafx not working complete

--- src/main/java/org/crazytracks/gui/sui/mainsound/CompatibilitySoundPlayer.java
+++ src/main/java/org/crazytracks/gui/sui/mainsound/CompatibilitySoundPlayer.java
@@ -23,19 +23,6 @@ public class CompatibilitySoundPlayer implements SUI {
         }
     }
 
-    public void playSound(String filePath) {
-        try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            volume.setValue(-20.0f);
-            clip.start();
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void stopMusic() {
         if (backgroundMusic != null && backgroundMusic.isRunning()) {
