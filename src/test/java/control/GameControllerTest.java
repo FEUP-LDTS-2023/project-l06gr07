d522c4b918108482fce53afab9ff8c4581846cd3
Daniel Basílio <dbasilio15@gmail.com>
2023-12-23 10:18:05 +0000

    fixed some bugs in the test

--- src/test/java/control/GameControllerTest.java
+++ src/test/java/control/GameControllerTest.java
@@ -3,6 +3,7 @@ package control;
 import org.crazytracks.Game;
 import org.crazytracks.control.GameController;
 import org.crazytracks.gui.GUI;
+import org.crazytracks.model.Track;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,22 +14,19 @@ public class GameControllerTest {
 
     @Test
     public void step() throws IOException {
-        // Arrange
-        GameController gameController = Mockito.mock(GameController.class);
+        GameController gameController = new GameController(new Track()) {
+            @Override
+            public void step(Game game, GUI.ACTION action, long time) throws IOException {
+                // not implemented
+            }
+        };
         Game mockedGame = Mockito.mock(Game.class);
         GUI.ACTION mockedAction = GUI.ACTION.NONE;
         long mockedTime = 100L;
-        Mockito.doNothing().when(gameController).step(mockedGame, mockedAction, mockedTime);
 
-        // Act
-        try {
-            gameController.step(mockedGame, mockedAction, mockedTime);
-        } catch (IOException e) {
-            Assertions.fail("IOException occurred during step execution: " + e.getMessage());
-        }
+        gameController.step(mockedGame, mockedAction, mockedTime);
 
-        // Assert
-        Mockito.verify(gameController).step(mockedGame, mockedAction, mockedTime);
+        Assertions.assertNotNull(gameController.getModel());
     }
 }
 
